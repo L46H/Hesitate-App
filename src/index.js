f3525b525c6784981d4b1d3156604612d061bd4f
Hub3rt65@gmail.com <Hubert65@gmail.com>
2022-04-13 18:24:07 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HesitateApp from './components/HesitateApp';
+import './styles/styles.scss';
+
+ReactDOM.render(<HesitateApp />, document.getElementById('root'));
